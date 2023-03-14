96d5c8548190a2103a38ae51eec5c13829382b98
ydemek <yasindemek75@gmail.com>
2023-03-14 04:14:12 +0300

    test

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.use(
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('Express on Vercel');
+    res.send('Express on Vercel test');
 });
 app.get('/createUser', createUserContoller);
 app.get('/creatCities', createCitiesController);
