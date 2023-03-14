36ca10532278b5ff59293587230594b0088ac46a
ydemek <yasindemek75@gmail.com>
2023-03-14 09:57:04 +0300

    config

--- client/src/services/config.js
+++ client/src/services/config.js
@@ -1,1 +1,2 @@
-export const API_URL = import.meta.env.API_URL ?? "https://weather-app-server-ruddy.vercel.app";
+// export const API_URL = import.meta.env.API_URL ?? "https://weather-app-server-ruddy.vercel.app";
+export const API_URL = import.meta.env.API_URL ?? "http://localhost:5000";
